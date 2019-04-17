82aa4279dfa43ca009d54873015963a9dab1058e
1n3ffbl3 <mizgier.m@gmail.com>
2019-04-17 12:06:38 +0200

    Adding styles to list component. Creating modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Modal.module.scss';
+
+const Modal = () => (
+	<div className={styles.wrapper}>
+		MODAL
+	</div>
+);
+
+export default Modal;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './index.css';
 import List from '../../components/List/List';
+import Modal from '../../components/Modal/Modal';
 
 class Root extends Component {
+	state = {
+		isModalOpen: true
+	}
+
 	render() {
+		const { isModalOpen } = this.state;
 		return (
 			<>
 				<List />
+				{isModalOpen && <Modal />}
 			</>
 
 

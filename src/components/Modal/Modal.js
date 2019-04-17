8bfa08d55b988f54eee027de4ef7f0227efb7573
1n3ffbl3 <mizgier.m@gmail.com>
2019-04-17 16:51:43 +0200

    Adding style to modal and close button

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,7 +5,7 @@ import Form from '../Form/Form';
 
 const Modal = ({ handleModal }) => (
 	<div className={styles.wrapper}>
-		<button onClick={handleModal}>x</button>
+		<button onClick={handleModal} className={styles.closeButton}>x</button>
 		<h2 className={styles.header}>ADD NEW ITEM</h2>
 		<Form />
 	</div>

100b87fad15e9fb62bcb8fe9d5ed6924290f5668
1n3ffbl3 <mizgier.m@gmail.com>
2019-04-19 08:10:37 +0200

    Adding style to item

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -2,31 +2,38 @@ import React from 'react';
 import styles from './ListItem.module.scss';
 import Checkbox from '../Checkbox/Checkbox';
 import AppContext from '../../context';
+import { MdClear } from 'react-icons/md';
 
 const ListItem = ({ name, quantity, isBought }) => (
 	<AppContext.Consumer>
 		{(context) => (
 			<li className={styles.wrapper}>
 				<div className={styles.subWrapper}>
-					<div className={styles.info}>
-						<h4>Name:</h4>
-						<h2 className={styles.itemName}>{name}</h2>
-					</div>
-					<div className={styles.info}>
-						<h4>Quantity:</h4>
-						<p className={styles.itemQuantity}>{quantity}</p>
+					<div className={styles.infoWrapper}>
+						<div className={styles.info}>
+							<h3>Name:</h3>
+							{/* <h3 className={styles.itemName}>{name}</h3> */}
+							<h3>{name}</h3>
+						</div>
+						<div className={styles.info}>
+							<h3>Quantity:</h3>
+							{/* <p className={styles.itemQuantity}>{quantity}</p> */}
+							<h3>{quantity}</h3>
+						</div>
 					</div>
 					<div className={styles.checkbox}>
 						<Checkbox name="completed"
 							checked={isBought ? true : false}
 							onChange={() => context.markAsCompleted(name)} />
 					</div>
+					<div className={styles.remove}>
+						<MdClear onClick={() => context.deleteItem(name)} />
+					</div>
 				</div>
-
 			</li>
 		)}
 
-	</AppContext.Consumer >
+	</AppContext.Consumer>
 );
 
 export default ListItem;
